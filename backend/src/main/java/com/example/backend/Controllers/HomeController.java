1d4f1c9ce8d9798129ca1f199a99df4e9ac496d9
Gizem Bilgiş <gizembilgis@gmail.com>
2026-05-20 12:44:19 +0300

    test6

--- backend/src/main/java/com/example/backend/Controllers/HomeController.java
+++ backend/src/main/java/com/example/backend/Controllers/HomeController.java
@@ -27,6 +27,6 @@ public class HomeController {
 
     @GetMapping("/home")
     public String getHome(){
-        return "Merhaba DevOps! İlk CI/CD Pipeline projemiz basariyla calisiyor. ve mutluyuz2233334444";
+        return "Merhaba DevOps! İlk CI/CD Pipeline projemiz basariyla calisiyor. ve mutluyuz2233334444555";
     }
 }
